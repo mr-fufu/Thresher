7983a2a383955cb4d036f0fd955be1886fb1b141
Jack <monkeymanp8@gmail.com>
2020-03-09 02:18:05 -0400

    Added Pointer
    pointer now moves with arrow keys, does not display correctly behind tiles, may be unfixable. tilemap height query WIP

--- Assets/GameManagementScript.cs
+++ Assets/GameManagementScript.cs
@@ -5,9 +5,15 @@ using UnityEngine.Tilemaps;
 
 public class GameManagementScript : MonoBehaviour
 {
-    public GameObject tilemap;
-    public GameObject highlight;
-    public GameObject objects;
+    public GameObject tilesGrid;
+    public GameObject effectsGrid;
+    public GameObject objectsGrid;
+
+    private Tilemap tiles;
+    private Tilemap effects;
+    private Tilemap objects;
+
+    public Tile pointerTile;
 
     public GameObject summonLibrary;
 
@@ -16,9 +22,25 @@ public class GameManagementScript : MonoBehaviour
 
     public string checkTileType;
 
+    public int[] pointerLocation;
+    public int[] mapSize;
+
+    public int checkMatrix1;
+    public int checkMatrix2;
+
     // Start is called before the first frame update
     void Start()
     {
+        tiles = tilesGrid.GetComponent<Tilemap>();
+        effects = effectsGrid.GetComponent<Tilemap>();
+        objects = objectsGrid.GetComponent<Tilemap>();
+
+        pointerLocation = new int[2] { 0, 15 };
+        mapSize = new int[2] { mapSizeX, mapSizeY };
+
+        showPointer();
+
+        /*
         for (int axis1 = 0; axis1 <= mapSizeX; axis1++)
         {
             for (int axis2 = 0; axis2 <= mapSizeX; axis2++)
@@ -26,26 +48,70 @@ public class GameManagementScript : MonoBehaviour
                 checkTileType = checkTile(axis1, axis2);
             }
         }
+        */
     }
 
-    string checkTile(int ax1, int ax2)
+    string checkTile(int[] typeCheckLocation)
     {
-        return (tilemap.GetComponent<Tilemap>().GetInstantiatedObject(new Vector3Int(ax1, ax2, 0)).GetComponent<tileObjectScript>().tileType);
+        return (tiles.GetInstantiatedObject(new Vector3Int(typeCheckLocation[0], typeCheckLocation[1], 0)).GetComponent<tileObjectScript>().tileType);
     }
 
-    int checkTileHeight(int axi1, int axi2)
+    int checkTileHeight(int[] heightCheckLocation)
     {
-        return Mathf.FloorToInt(tilemap.GetComponent<Tilemap>().GetInstantiatedObject(new Vector3Int(axi1, axi2, 0)).transform.position.y);
+        return Mathf.FloorToInt(tiles.GetCellCenterLocal(new Vector3Int(heightCheckLocation[0], heightCheckLocation[1], 0)).y);
     }
 
-    void changeTile(int changeTo1, int changeTo2, Tile changeToTile)
+    void changeTile(int[] changeLocation, Tile changeToTile)
     {
-        tilemap.GetComponent<Tilemap>().SetTile(new Vector3Int(changeTo1, changeTo2, 0), changeToTile);
+        tiles.SetTile(new Vector3Int(changeLocation[0],changeLocation[1], 0), changeToTile);
+    }
+
+    void showPointer()
+    {
+        effects.ClearAllTiles();
+
+        effects.SetTile(new Vector3Int(pointerLocation[0], pointerLocation[1], 0), pointerTile);
+        effects.SetTransformMatrix(new Vector3Int(pointerLocation[0], pointerLocation[1], 0), 
+            tiles.GetTransformMatrix(new Vector3Int(pointerLocation[0], pointerLocation[1], 0)));
+
+        Debug.Log(checkTileHeight(pointerLocation));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (pointerLocation[0] < mapSize[0])
+            {
+                pointerLocation[0]++;
+                showPointer();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (pointerLocation[0] > 0)
+            {
+                pointerLocation[0]--;
+                showPointer();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (pointerLocation[1] < mapSize[1])
+            {
+                pointerLocation[1]++;
+                showPointer();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (pointerLocation[1] > 0)
+            {
+                pointerLocation[1]--;
+                showPointer();
+            }
+        }
+
     }
 }
